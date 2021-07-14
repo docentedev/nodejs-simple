0bd5f659253b62476b442070502692f57f8ca520
User <user@Users-MacBook-Air.local>
2021-07-14 02:27:05 -0400

    Conf port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const getTodos = require('./get-todos')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 5000
 
 app.use('/static', express.static('public'))
 
